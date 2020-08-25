ef271995ca0e2a935845d86d5398ad2bf874b5b6
smallker <wahyu.smallker@gmail.com>
2020-08-25 14:09:39 +0700

    Add shutdown and restart system

--- NodeMCU/src/main.cpp
+++ NodeMCU/src/main.cpp
@@ -1,18 +1,33 @@
-#define BLYNK_PRINT Serial
+// #define BLYNK_PRINT Serial
+// #define BLYNK_DEBUG
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <ArduinoJson.h>
 char auth[] = "Tdvwhxra_-q2XxcdJfnb5MjkhL4k1hGB";
 char ssid[] = "y";
 char pass[] = "11111111";
 
 BlynkTimer timer;
-
+BLYNK_WRITE(V2)
+{
+  Serial.write('b');
+}
+BLYNK_WRITE(V3)
+{
+  Serial.write('a');
+}
 void myTimerEvent()
 {
   // Blynk.virtualWrite(V5, millis() / 1000);
   if(Serial.available()>0){
-    String value = Serial.readStringUntil(',');
-    Blynk.virtualWrite(V0,value);
+    DynamicJsonDocument output(1024);
+    String data = Serial.readStringUntil('\n');
+    deserializeJson(output, data);
+    String load = output["load"];
+    String temp = output["temp"];
+    Blynk.virtualWrite(V0,load);
+    Blynk.virtualWrite(V1,temp);
   }
 }
 void setup()
@@ -20,6 +35,7 @@ void setup()
   Serial.begin(9600);
   Blynk.begin(auth, ssid, pass);
   timer.setInterval(100L, myTimerEvent);
+  pinMode(D2, INPUT_PULLUP);
 }
 
 void loop()
--- vs2019/ConsoleApplication1/Program.cs
+++ vs2019/ConsoleApplication1/Program.cs
@@ -7,7 +7,7 @@ using OpenHardwareMonitor.Hardware;
 using System.Diagnostics;
 using System.IO.Ports;
 using System.Threading;
-
+using System.Management;
 
 namespace Get_CPU_Temp5
 {
@@ -43,38 +43,82 @@ namespace Get_CPU_Temp5
                     {
                         if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
                             return int.Parse(computer.Hardware[i].Sensors[j].Value.ToString());
-                            //Console.WriteLine("Temp => "+computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                        //Console.WriteLine("Temp => "+computer.Hardware[i].Sensors[j].Name + ":" + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
                     }
                 }
             }
             computer.Close();
             return 0;
-            
+
+        }
+        static void Shutdown()
+        {
+            ManagementBaseObject mboShutdown = null;
+            ManagementClass mcWin32 = new ManagementClass("Win32_OperatingSystem");
+            mcWin32.Get();
+
+            // You can't shutdown without security privileges
+            mcWin32.Scope.Options.EnablePrivileges = true;
+            ManagementBaseObject mboShutdownParams =
+                     mcWin32.GetMethodParameters("Win32Shutdown");
+
+            // Flag 1 means we want to shut down the system. Use "2" to reboot.
+            mboShutdownParams["Flags"] = "1";
+            mboShutdownParams["Reserved"] = "0";
+            foreach (ManagementObject manObj in mcWin32.GetInstances())
+            {
+                mboShutdown = manObj.InvokeMethod("Win32Shutdown",
+                                               mboShutdownParams, null);
+            }
+        }
+        static void Restart()
+        {
+            var cmd = new ProcessStartInfo("shutdown.exe", "-r -t 0");
+            cmd.CreateNoWindow = true;
+            cmd.UseShellExecute = false;
+            cmd.ErrorDialog = false;
+            Process.Start(cmd);
         }
         static void Main(string[] args)
         {
             PerformanceCounter cpuCounter;
-            PerformanceCounter ramCounter;
+            //PerformanceCounter ramCounter;
             SerialPort _serialPort;
             cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
-            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            //ramCounter = new PerformanceCounter("Memory", "Available MBytes");
             _serialPort = new SerialPort();
-            _serialPort.PortName = "COM9";//Set your board COM
+            _serialPort.PortName = "COM9";
             _serialPort.BaudRate = 9600;
             _serialPort.Open();
             while (true)
             {
                 int temperature = GetSystemInfo();
                 int cpuLoad = (int)cpuCounter.NextValue();
-                Console.WriteLine("Temp => :" + GetSystemInfo());
-                Console.WriteLine("CPU Usage => "+(int)cpuCounter.NextValue() + "%");
-                //string a = _serialPort.ReadExisting();
-                //string a = _serialPort.Write("a");
-                //_serialPort.Write("a");
-                //Thread.Sleep(1000);
-                //_serialPort.Write("b");
-                Thread.Sleep(1000);
-                Console.Clear();
+                string cmd = _serialPort.ReadExisting();
+                string date = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
+                Console.WriteLine("\r\n\r\n");
+                Console.WriteLine(date);
+                Console.WriteLine("Temp         : " + GetSystemInfo());
+                Console.WriteLine("CPU Usage    : " + cpuLoad + "%");
+                if (cmd.Contains("a"))
+                {
+                    Console.WriteLine("Command Shutdown Received");
+                    Shutdown();
+                    break;
+                }
+                if (cmd.Contains("b"))
+                {
+                    Console.WriteLine("Command Restart Received");
+                    Restart();
+                    break;
+                }
+                _serialPort.Write("{\"load\":" + cpuLoad.ToString() + ",\"temp\":" + temperature.ToString() + "}");
+                _serialPort.Write("\n");
+                Thread.Sleep(500);
+            }
+            while (true)
+            {
+                Thread.Sleep(500);
             }
         }
     }
